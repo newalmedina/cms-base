eb96960cf329d456567081218a0854e3272118a0
newal medina <ing.newal.medina@gmail.com>
2024-07-13 18:53:47 +0200

    test

---
+++ resources/lang/es/patient-medical-studies/admin_lang.php
@@ -0,0 +1,60 @@
+<?php
+
+
+return [
+    'patient-medical-studies' => 'Estudios médicos',
+    'patient-medical-studies-export' => 'estudios_medicos',
+    'patients' => "Pacientes",
+    'todos' => 'Todos los estudios médicos',
+    'patient-medical-study' => 'Estudio médico',
+    'list' => 'Listado de estudios médicos',
+    'new' => 'Nuevo estudio médico',
+    'edit' => 'Editar estudio médico',
+    'show' => 'Visualizar estudio médico',
+    'patient-medical-studies-management' => 'Gestión de estudios médicos',
+    'created_at' => 'Creado el:',
+
+
+
+    'fields' => [
+
+        'id' => 'Id',
+        'id_helper' => 'Introduce id',
+        'id_required' => 'El campo id es obligatorio',
+
+        'date' => 'Fecha',
+        'date_helper' => 'Introduce fecha',
+        'date_required' => 'El campo fecha es obligatorio',
+
+        'created_by' => 'Creado por',
+        'created_by_helper' => 'Introduce creado por',
+        'created_by_required' => 'El campo creado por es obligatorio',
+
+
+
+        'center' => 'Centro',
+        'center_helper' => 'Introduce centro',
+        'center_required' => 'El campo centro es obligatorio',
+
+        'centers' => 'Centros',
+        'centers_helper' => 'Introduce centros',
+        'centers_required' => 'El campo centros es obligatorio',
+
+        'start_date' => 'Fecha inicial',
+        'start_date_helper' => 'Introduce fecha inicial',
+        'start_date_required' => 'El campo fecha inicial es obligatorio',
+
+        'end_date' => 'Fecha final',
+        'end_date_helper' => 'Introduce fecha final',
+        'end_date_required' => 'El campo fecha final es obligatorio',
+
+        'patient' => 'Paciente',
+        'comment' => 'Comentario',
+        'description' => 'Descripción',
+        'description_helper' => 'Introduce descripción',
+        'description_required' => 'El campo descripción es obligatorio',
+
+    ],
+
+
+];
